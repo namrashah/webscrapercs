718d3ddc5d126b9b785f17147cdb99ae30f16a16
Venkatakrishnan Srinivasaraj <venkatakrishnan111@gmail.com>
2018-11-01 19:44:05 -0500

    Updated with all table names

--- ClearDataInDb.sql
+++ ClearDataInDb.sql
@@ -1,4 +1,12 @@
-delete from url where id>285;
+delete from amenity where id > 0;
+delete from amenitytype where id > 0;
+delete from apartments where id > 0;
+delete from expenses where id > 0;
+delete from expensetype where id > 0;
+delete from ntpi where id > 0;
+delete from review where id > 0;
+delete from school where id > 0;
 delete from property where id > 0;
 delete from propertytype where id > 0;
+delete from url where id>285;
 update url set status=0 where id = 285;
